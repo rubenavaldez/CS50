04c237878e2fff8b51b03f2f1ad7f1c03e37d3b2
rubenavaldez <rubenavaldez@yahoo.com>
2024-05-31 15:21:11 -0700

    added return values to function2

---
+++ Functions2.py
@@ -0,0 +1,19 @@
+
+# def main():
+#     print("Hello, world")
+#     print("This is CS50P.")
+
+
+# main()
+
+'''
+return values 
+'''
+
+def greet(input):
+    if "hello" in input:
+        return "hello, there"
+    else:
+        return "I'm not sure what you mean"
+
+print(greet("hello, computer"))
