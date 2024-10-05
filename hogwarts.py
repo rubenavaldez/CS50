1e9a1b98f98c0669035c5aaf3e3ede7bcd0a5353
rubenavaldez <rubenavaldez@yahoo.com>
2024-10-05 12:20:52 -0700

    finished hogwarts

--- hogwarts.py
+++ hogwarts.py
@@ -19,15 +19,47 @@
 
 # dictionaries 
 
-students = {
-    "Hermione": "Gryffindor",
-    "Harry": "Gryffindor",
-    "Ron": "Gryffindor",
-    "Draco":"Slytherine" 
-}
+# students = {
+#     "Hermione": "Gryffindor",
+#     "Harry": "Gryffindor",
+#     "Ron": "Gryffindor",
+#     "Draco":"Slytherine" 
+# }
 
 # print(students["Hermione"])
 
-for student in students:
-    print(student,students[student])
+# for student in students:
+#     print(student,students[student], sep=", House ")
 
+# More Data
+students = [
+    {
+        'name': "Hermione",
+        'house': "Gryffindor",
+        'patronus': "Otter"
+    },
+    {
+        'name': "Harry",
+        'house': "Gryffindor",
+        'patronus': "Stag"
+    },
+    {
+        'name': "Ron",
+        'house': "Gryffindor",
+        'patronus': "Jack Russle Terrier"
+    },
+    {
+        'name': "Draco",
+        'house': "Slytherine",
+        'patronus': None
+    },
+]
+
+# for student in students:
+#     print(student["name"], student["house"], student["patronus"], sep=", ")
+
+# with a nested loop
+# for student in students:
+#     for key in student:
+#         print(key, student[key])
+#     print("\n")
